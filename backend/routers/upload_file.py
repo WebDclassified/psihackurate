ca224aec3b12dfab3af5f41962f7df547c6d08a8
Prabhat Teotia <112494157+WebDclassified@users.noreply.github.com>
2025-02-15 16:12:50 +0530

    Delete backend directory

--- backend/routers/upload_file.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter, UploadFile, File
-
-router = APIRouter()
-
-@router.post("/")
-async def upload_file(file: UploadFile = File(...)):
-    print(f"Received file: {file.filename}")  # Debug log
-    return {"message": "Image upload success", "filename": file.filename}
